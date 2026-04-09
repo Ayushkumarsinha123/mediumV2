d58c3c325786e48072f84b8505bba2cc9397c9ac
Ayushkumarsinha123 <itsharshit0005@gmail.com>
2026-04-09 21:42:33 +0530

    feat:Implemented the signin routes and middleware for this path /api/v1/blog/*

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign,verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
@@ -10,9 +10,37 @@ const app = new Hono<{
   };
 }>();
 
+app.use('/api/v1/blog/*', async (c, next ) => {
+    const header = c.req.header("authorization") || "";
+
+    if (!header || !header.startsWith("Bearer ")) {
+    c.status(401);
+    return c.json({ error: "Unauthorized: Missing or invalid header" });
+  }
+  
+    // Bearer Token => ['bearer', 'token']
+    const token = header.split(' ')[1];
+  try {
+    // verify the token 
+    const payload = await verify(token, c.env.JWT_SECRET);
+
+    if (payload && payload.id) {
+      // @ts-ignore 
+      c.set("userId", payload.id);
+      await next();
+    } else {
+      c.status(403);
+      return c.json({ error: "Unauthorized: Invalid payload" });
+    }
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Unauthorized: Token verification failed" });
+  }
+})
+
 app.post("/api/v1/user/signup", async (c) => {
   const prisma = new PrismaClient({
-    accelerateUrl: c.env.DATABASE_URL,
+    accelerateUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
 
   try {
@@ -42,5 +70,42 @@ app.post("/api/v1/user/signup", async (c) => {
     });
   }
 });
+
+app.post('/api/v1/user/signin', async (c) => {
+  const prisma = new PrismaClient({
+    accelerateUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const body = await c.req.json();
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (!user || user.password !== body.password) {
+      c.status(403);
+      return c.json({ error: "Invalid credentials" });
+    }
+
+    
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({
+      jwt: token,
+      message: "logged in",
+    });
+  } catch (e) {
+    console.error("Signin Error:", e);
+    
+    c.status(403);
+    return c.json({ 
+      error: "error while signing in",
+      details: e instanceof Error ? e.message : e 
+    });
+  }
+} )
 
 export default app;
