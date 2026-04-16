f1ed27ae0369077db694c01fa7c5caa428d58b56
Ayushkumarsinha123 <itsharshit0005@gmail.com>
2026-04-16 20:02:50 +0530

    feat:build the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import './App.css'
+import Home from './pages/Home';
 
 function App() {
-
-  return (
+    return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
